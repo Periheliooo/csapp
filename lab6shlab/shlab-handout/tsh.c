5208ef42233573e1f04a9a4ac605dfb33df7d13e
Periheliooo <wuzhengyang2007@sjtu.edu.cn>
2026-07-14 14:05:14 +0800

    shlab build structure1

--- lab6shlab/shlab-handout/tsh.c
+++ lab6shlab/shlab-handout/tsh.c
@@ -165,6 +165,38 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS];
+    int bg;
+
+    bg = parseline(cmdline, argv);
+
+    if (argv[0] == NULL)
+        return ;
+    
+    if (builtin_cmd(argv[0])) {
+        return ;
+    }
+
+    pid_t pid = fork();
+    
+    if (pid < 0) {
+        perror("fork");
+    }
+
+    if (pid == 0) {
+        execve(argv[0], argv, environ);
+        perror("execve");
+        _exit(127);
+    } else {
+        waitpid(pid, )
+    }
+    
+
+    if (bg == 0) {
+
+    } else {
+
+    }
     return;
 }
 
@@ -231,6 +263,15 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
+    if (strcmp(argv[0], "quit") == 0) {
+        exit(0);
+    } else if (strcmp(argv[0], "bg") == 0 || strcmp(argv[0], "fg") == 0) {
+        do_bgfg();
+    } else if (strcmp(argv[0], "jobs") == 0) {
+        listjobs(jobs);
+    } else {
+
+    }
     return 0;     /* not a builtin command */
 }
 
